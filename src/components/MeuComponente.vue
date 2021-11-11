00185e4a312025663b4d4be97e74eb1a4a17aa8b
Fabiano Mendes <fabianomendesoficial@gmail.com>
2021-11-11 13:06:49 -0300

    update aula dia 10

--- src/components/MeuComponente.vue
+++ src/components/MeuComponente.vue
@@ -2,23 +2,56 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Mochiy+Pop+One&display=swap" rel="stylesheet">
-  <div class="container-md">
-    <h4 class="mt-4">Selecione os produtos</h4>
-    <section class="itens">
-      <div class="card m-3 pt-1" style="width: 18rem;" v-for="produto in produtos" :key="produto.nome" :class="{ativo :produto.ativo}" @click="produto.ativo = !produto.ativo">
-        <img class="card-img-top img" :src="produto.foto" alt="Card image cap">
-        <div class="prodIndis" v-if="produto.qtd < 1">
-          <span>Produto indisponível</span>
-        </div>
-        <div class="card-body">
-          <h5 class="card-title" style="font-weight: bold">{{ produto.nome }}</h5>
-          <p class="card-text">R$ {{ produto.valor }}</p>
-          <button class="btn btn-primary btn-sm">-</button>
-          <span class="mx-2">1</span>
-          <button class="btn btn-primary btn-sm">+</button>
-        </div>
+  <div class="container-lg">
+    <div class="row">
+      <div class="col col-md-9">
+        <h4 class="mt-4">Selecione os produtos</h4>
+        <section class="itens">
+          <div class="card m-3 pt-1" style="width: 18rem;" v-for="produto in produtos" :key="produto.nome" :class="{ativo :produto.ativo}" @click="produto.ativo = !produto.ativo">
+            <img class="card-img-top img" :src="produto.foto" alt="Card image cap">
+            <div class="prodIndis" v-if="produto.qtd < 1">
+              <span>Produto indisponível</span>
+            </div>
+            <div class="card-body">
+              <h5 class="card-title" style="font-weight: bold">{{ produto.nome }}</h5>
+              <p class="card-text">R$ {{ produto.valor }}</p>
+              <div class="quantidade" :class="{esconder :!produto.ativo}">
+                <button @click.stop="produto.count--" :disabled="produto.count <= 1 || produto.qtd == 0" class="btn btn-primary btn-sm">-</button>
+                <span class="mx-2">{{ produto.count }}</span>
+                <button @click.stop="produto.count++" :disabled="produto.qtd == 0" class="btn btn-primary btn-sm">+</button>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
-    </section>
+
+      <div class="col col-md-3" v-show="total() > 0">
+        <strong>Resumo do pedido</strong>
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th>Qtd</th>
+              <th>Item</th>
+              <th>Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="produto in produtos" :key="produto.nome">
+              <td v-if="produto.ativo">{{ produto.count}}</td>
+              <td v-if="produto.ativo">{{ produto.nome }}</td>
+              <td v-if="produto.ativo">{{ (produto.count * produto.valor).toFixed(2).toString().replace('.', ',') }}</td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <th></th>
+              <th>Total</th>
+              <th>{{ total().toFixed(2).toString().replace('.', ',') }}</th>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -33,64 +66,82 @@ export default {
           "nome": "Desmaia Cabelo",
           "valor": 10.00,
           "ativo": false,
-          "qtd": 0
+          "qtd": 0,
+          "count": 1
         },
         {
           "foto": "assets/image2.jpg",
           "nome": "Kit Inoar",
           "valor": 15.00,
           "ativo": false,
-          "qtd": 10
+          "qtd": 10,
+          "count": 1
         },
         {
           "foto": "assets/image3.jpg",
           "nome": "Olive Oil",
           "valor": 23.54,
           "ativo": false,
-          "qtd": 0
+          "qtd": 0,
+          "count": 1
         },
         {
           "foto": "assets/image4.jpg",
           "nome": "Repos",
           "valor": 30.00,
           "ativo": false,
-          "qtd": 10
+          "qtd": 10,
+          "count": 1
         },
         {
           "foto": "assets/image5.png",
           "nome": "Cachos",
           "valor": 30.00,
           "ativo": false,
-          "qtd": 5
+          "qtd": 5,
+          "count": 1
         },
         {
           "foto": "assets/image6.jpg",
           "nome": "Mealiza",
           "valor": 30.00,
           "ativo": false,
-          "qtd": 50
+          "qtd": 50,
+          "count": 1
         },
         {
           "foto": "assets/image7.jpg",
           "nome": "Mealiza",
           "valor": 30.00,
           "ativo": false,
-          "qtd": 0
+          "qtd": 0,
+          "count": 1
         },
         {
           "foto": "assets/image8.png",
           "nome": "Mealiza",
           "valor": 30.00,
           "ativo": false,
-          "qtd": 100
+          "qtd": 100,
+          "count": 1
         }
       ]
+    }
+  },
+  methods: {
+    total() {
+      let total = 0
+      this.produtos.forEach(function(item) {
+        if(item.ativo){
+          total += item.valor * item.count
+        }
+      })
+      return total
     }
   }
 }
 
 </script>
-
 
 <style scoped>
   .container-md {
@@ -112,11 +163,23 @@ export default {
     flex-wrap: wrap;
   }
   .card {
+    position: relative;
     flex-basis: 20%;
+    min-width: 200px;
+    box-shadow: 5px 5px 10px 5px rgba(0, 0, 0, 0.226);
+    padding-bottom: 10px;
   }
   .prodIndis{
     padding-left: 6px;
     color: #FFF;
     background: rgb(109, 109, 109);
+  }
+
+  .esconder{
+    visibility: hidden;
+  }
+  .quantidade{
+    position: absolute;
+    bottom: 10px;
   }
 </style>
